2782b9322ff1c6bddf910c156d7d7cdf6bb5743e
buckocee <buckocee@gmail.com>
2018-04-22 19:27:16 -0400

    Initial commit

---
+++ src/main/java/com/silvershield/SilvershieldApplication.java
@@ -0,0 +1,12 @@
+package com.silvershield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilvershieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilvershieldApplication.class, args);
+	}
+}
