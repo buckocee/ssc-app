b4e281874c21921d2188b8c5ed7bf342f9043f55
buckocee <buckocee@gmail.com>
2018-04-25 16:26:31 -0400

    Added Dockerfile contents, auth configurations

--- src/main/java/com/silvershield/ssc/SscApplication.java
+++ src/main/java/com/silvershield/ssc/SscApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class SscApplication {
 
     public static void main(String[] args) {
