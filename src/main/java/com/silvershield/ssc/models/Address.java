1952a1a437dc2daf43aed877e67973f4d2d786d8
buckocee <buckocee@gmail.com>
2018-04-26 07:03:21 -0400

    Added Address POJO

---
+++ src/main/java/com/silvershield/ssc/models/Address.java
@@ -0,0 +1,30 @@
+package com.silvershield.ssc.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "street_address")
+    private String streetAddress;
+
+    @Column(name = "apt_number")
+    private String aptNumber;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+}
