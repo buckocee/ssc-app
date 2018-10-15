bb12fa357950a807797c40b2c4c8de08800236eb
buckocee <buckocee@gmail.com>
2018-10-15 15:33:35 -0700

    @Repository annotation added

--- src/main/java/com/silvershield/ssc/repos/UserRepository.java
+++ src/main/java/com/silvershield/ssc/repos/UserRepository.java
@@ -2,6 +2,8 @@ package com.silvershield.ssc.repos;
 
 import com.silvershield.ssc.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
