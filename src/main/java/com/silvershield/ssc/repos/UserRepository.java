b56c5b69052e46b59eb8d91f66feaac265b8a531
buckocee <buckocee@gmail.com>
2018-10-15 15:37:35 -0700

    Removed duplicate repository

--- src/main/java/com/silvershield/ssc/repos/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.silvershield.ssc.repos;
-
-import com.silvershield.ssc.auth.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
